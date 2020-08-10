4236f9ebaf9d92000403c7da03c5f3b44f672e2c
John Lennie <johnanthonylennie@gmail.com>
2020-08-10 16:08:04 -0400

    moved global and reset styles to new _app page

--- pages/_document.js
+++ pages/_document.js
@@ -15,8 +15,6 @@ class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500&display=swap" rel="stylesheet" />
           <link rel="icon" href="/favicon.png" type="image/png" />
         </Head>
-        <style jsx global>{ reset }</style>
-        <style jsx global>{ globals }</style>
         <body>
           <Main />
           <NextScript />
