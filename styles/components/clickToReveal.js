fd901960f671cf0a105537ea74475bbc2247a648
John Lennie <johnanthonylennie@gmail.com>
2020-08-11 13:02:26 -0400

    fixed ctr height issue

--- styles/components/clickToReveal.js
+++ styles/components/clickToReveal.js
@@ -9,10 +9,12 @@ export const clickToRevealStyles = css.global`
   .ctr-desc.hidden {
     opacity: 0;
     top: -10px;
+    height: 0;
   }
   .ctr-desc {
     position: relative;
     top: 0;
+    height: auto;
     opacity: 1;
     transition: all 0.3s ease;
   }
