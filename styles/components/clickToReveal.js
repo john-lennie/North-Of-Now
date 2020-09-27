58276a01d8ddd4c1f5dea98a3a2c085848dd8fe8
John Lennie <johnanthonylennie@gmail.com>
2020-09-27 14:16:12 -0400

    styling changes

--- styles/components/clickToReveal.js
+++ styles/components/clickToReveal.js
@@ -3,11 +3,14 @@ import css from 'styled-jsx/css'
 export const clickToRevealStyles = css.global`
   .ctr-title {
     cursor: pointer;
+    position: relative;
+    z-index: 2;
     text-decoration: underline;
   }
   .ctr-desc.hidden {
     opacity: 0;
     top: -10px;
+    z-index: 1;
     height: 0;
   }
   .ctr-desc {
